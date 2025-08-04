0a8fd33bddf141c1267cd7e7b573f6a5f70656c7
sn0w <105451515+sn0w12@users.noreply.github.com>
2025-08-04 23:38:23 +0200

    Improve file serving

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -109,7 +109,7 @@ func main() {
     authRoute(mux, "/server/metrics", middleware.GlobalRateLimit, Cache5Min, handlers.GetMetricsHandler)
 
     // Static file serving for uploads
-    mux.Handle("/uploads/", middleware.CacheControl(24*time.Hour, "public")(http.StripPrefix("/uploads/", http.FileServer(http.Dir("uploads/"))).ServeHTTP))
+    mux.Handle("/uploads/", middleware.CacheControl(24*time.Hour, "public")(middleware.SecureStaticFileServer("uploads").ServeHTTP))
     // Server icon endpoint
     publicRoute(mux, "/icon", middleware.GlobalRateLimit, Cache24Hour, handlers.GetServerIconHandler)
     // WebSocket endpoint
